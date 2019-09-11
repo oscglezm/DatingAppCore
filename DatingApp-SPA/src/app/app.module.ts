a5533965ea2512ec4eed3926e5de99ce73c3fbd2
oscargm <e321352@miamidade.gov>
2019-09-11 17:19:46 -0400

    v1.6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,25 +10,34 @@ import { NavComponent} from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       RegisterComponent,
-      HomeComponent
+      HomeComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      HttpClientModule  // Addreference\\r\\nFormsModule
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
-     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: ErrorInterceptor,
+         multi: true
       }, 
    ],
    bootstrap: [
